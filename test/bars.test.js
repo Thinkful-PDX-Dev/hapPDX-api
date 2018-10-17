eb64e614d4c4319dadb3c1fea4c1ef476d4310d4
Courtney Adams <cadams8238@gmail.com>
2018-10-17 13:28:30 -0700

    added basic test for 200 response for POST request at '/bars'

--- test/bars.test.js
+++ test/bars.test.js
@@ -50,7 +50,6 @@ describe('Testing "/bars" endpoints', () => {
 		});
 	});
 
-	//this part not committed yet
 	describe("GET /bars/:id", () => {
 		it("Should correctly return one bar", () => {
 			let barData;
@@ -69,6 +68,36 @@ describe('Testing "/bars" endpoints', () => {
 					expect(res.body.name).to.equal(barData.name);
 					expect(res.body.address).to.equal(barData.address);
 					expect(res.body.hours).to.equal(barData.hours);
+				});
+		});
+	});
+
+	describe("POST /bars", () => {
+		it("Should return a new bar when given valid inputs", () => {
+			const newBar = {
+				name: "Bar",
+				address: "123 Easy St",
+				hours: "Open 24/7"
+			};
+			let body;
+
+			return chai
+				.request(app)
+				.post("/bars")
+				.send(newBar)
+				.then(res => {
+					body = res.body;
+					expect(res).to.have.status(201);
+					expect(res).to.be.json;
+					expect(body).to.be.an("object");
+					expect(res.body).to.contain.keys("id", "name", "address", "hours");
+					return Bar.findById(body.id);
+				})
+				.then(data => {
+					expect(body.id).to.equal(data.id);
+					expect(body.name).to.equal(data.name);
+					expect(body.address).to.equal(data.address);
+					expect(body.hours).to.equal(data.hours);
 				});
 		});
 	});
