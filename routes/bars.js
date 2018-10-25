908caeeceee3f5e1faa994ba78b5b923338d38e6
Courtney Adams <cadams8238@gmail.com>
2018-10-25 16:27:39 -0700

    Revert "refactored routing to happen in index.js file in routes/ directory. Set up basic message when home endpoint / is accessed to let user know the server is up and running"
    This reverts commit a307bf78113e7037b40b8b6a36c871594c347f71.

--- routes/bars.js
+++ routes/bars.js
@@ -1,126 +1,129 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const Bar = require("../models");
+const Bar = require('../models');
 
 const router = express.Router();
 
+
 //get data for all bars
-router.get("/bars", (req, res) => {
-	Bar.find()
-		.then(results => {
-			//serialize each response
-			res.json(results.map(bar => bar.serialize()));
-			// res.json(results);
-		})
-		.catch(err => {
-			//error if request fails
-			console.error(err);
-			res.status(500).json({ error: "bad bad bad" });
-		});
+router.get('/bars', (req, res) => {
+  Bar.find()
+    .then(results => {
+      //serialize each response
+      res.json(results.map(bar => bar.serialize()));
+      // res.json(results);
+
+    })
+    .catch(err => {
+      //error if request fails
+      console.error(err);
+      res.status(500).json({ error: 'bad bad bad' });
+    });
 });
 
 //get data for single bar by id
-router.get("/bars/:id", (req, res) => {
-	Bar.findById(req.params.id)
-		.then(bar => {
-			res.json(bar.serialize());
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json({ error: "ughhhhhhhh no no" });
-		});
+router.get('/bars/:id', (req, res) => {
+  Bar.findById(req.params.id)
+    .then(bar => {
+      res.json(bar.serialize());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'ughhhhhhhh no no' });
+    });
 });
 
 //post bar data
-router.post("/bars", (req, res) => {
-	//required fields for post
-	let requiredFields = ["name", "address", "hours"];
-	//check if each required field is present. If a field is not present, return an error message
-	for (var i = 0; i < requiredFields.length; i++) {
-		let field = requiredFields[i];
-		if (!field) {
-			return res.status(400).json({ error: "missing field in request body" });
-		}
-	}
-	//create new data object
-	Bar.create({
-		name: req.body.name,
-		address: req.body.address,
-		hours: req.body.hours,
-		description: req.body.description,
-		neighborhood: req.body.neighborhood,
-		phone: req.body.phone,
-		rating: req.body.rating,
-		review: req.body.review,
-		link: req.body.link,
-		img: req.body.img,
-		patio: req.body.patio
-	})
-		.then(newBar => {
-			res.status(201).json(newBar.serialize());
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json({ error: "ughhhhhhhh no no" });
-		});
+router.post('/bars', (req, res) => {
+  //required fields for post
+  let requiredFields = ['name', 'address', 'hours'];
+  //check if each required field is present. If a field is not present, return an error message
+  for (var i = 0; i < requiredFields.length; i++) {
+    let field = requiredFields[i];
+    if (!field) {
+      return res.status(400).json({ error: 'missing field in request body' });
+    }
+  }
+  //create new data object
+  Bar.create({
+    name: req.body.name,
+    address: req.body.address,
+    hours: req.body.hours,
+    description: req.body.description,
+    neighborhood: req.body.neighborhood,
+    phone: req.body.phone,
+    rating: req.body.rating,
+    review: req.body.review,
+    link: req.body.link,
+    img: req.body.img,
+    patio: req.body.patio,
+  })
+    .then(newBar=> {
+      res.status(201).json(newBar.serialize());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'ughhhhhhhh no no' });
+    });
 });
 
 //update bar data object
-router.put("/bars/:id", (req, res) => {
-	const { id } = req.params;
-	let {
-		name,
-		address,
-		hours,
-		description,
-		neighborhood,
-		phone,
-		rating,
-		review,
-		price,
-		link,
-		img,
-		patio
-	} = req.body;
+router.put('/bars/:id', (req, res) => {
+  const { id } = req.params;
+  let { 
+    name, 
+    address, 
+    hours, 
+    description,
+    neighborhood,
+    phone,
+    rating,
+    review,
+    price,
+    link,
+    img,
+    patio   
+  } = req.body;
+  
+  const updateBar = {};
 
-	const updateBar = {};
+  if(name) updateBar.name = name;
+  if(address) updateBar.address = address;
+  if(hours) updateBar.hours = hours; 
+  if(description) updateBar.description = description;
+  if(neighborhood) updateBar.neighborhood = neighborhood;
+  if(phone) updateBar.phone = phone;
+  if(rating) updateBar.rating = rating;
+  if(review) updateBar.review = review;
+  if(price) updateBar.price = price;
+  if(link) updateBar.link = link;
+  if(img) updateBar.img = img;
+  if(patio) updateBar.patio = patio;
 
-	if (name) updateBar.name = name;
-	if (address) updateBar.address = address;
-	if (hours) updateBar.hours = hours;
-	if (description) updateBar.description = description;
-	if (neighborhood) updateBar.neighborhood = neighborhood;
-	if (phone) updateBar.phone = phone;
-	if (rating) updateBar.rating = rating;
-	if (review) updateBar.review = review;
-	if (price) updateBar.price = price;
-	if (link) updateBar.link = link;
-	if (img) updateBar.img = img;
-	if (patio) updateBar.patio = patio;
 
-	Bar.findByIdAndUpdate({ _id: id }, updateBar, { new: true })
-		.then(result => {
-			res.status(201).json(result);
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json({ error: "ughhhhhhhh no no" });
-		});
+  Bar.findByIdAndUpdate({_id: id}, updateBar, { new: true })
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'ughhhhhhhh no no' });
+    });
 });
 
 //delete bar data object
-router.delete("/bars/:id", (req, res) => {
-	Bar.findByIdAndRemove(req.params.id)
-		.then(() => {
-			res.status(204).json({ message: "success, my friend!" });
-		})
-		.catch(err => {
-			console.error(err);
-			res.status(500).json({ error: "ughhhhhhhh no no" });
-		});
+router.delete('/bars/:id', (req, res) => {
+  Bar.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: 'success, my friend!' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'ughhhhhhhh no no' });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,72 +1,70 @@
 "use strict";
-require("dotenv").config();
+require('dotenv').config();
 
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 const { DATABASE_URL, PORT, CLIENT_ORIGIN } = require("./config");
 
 const app = express();
 
 const mongoose = require("mongoose");
-// const router = express.Router();
+const router = express.Router();
 
 mongoose.Promise = global.Promise;
 
-const routeApp = require("./routes/index");
+//ROUTES
+const barsRouter = require('./routes/bars');
 
 // CORS
 app.use(
-	cors({
-		origin: CLIENT_ORIGIN
-	})
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
 app.use(express.json());
 
-// Routes to Index of all Endpoints
-app.use(routeApp);
+//Endpoints
+app.use('/', barsRouter);
 
 let server;
 
 // connect to database, then start the server
 function runServer(databaseUrl, port = PORT) {
-	return new Promise((resolve, reject) => {
-		mongoose.connect(
-			databaseUrl,
-			err => {
-				if (err) {
-					return reject(err);
-				}
-				server = app
-					.listen(port, () => {
-						console.log(`Your app is listening on port ${port}`);
-						resolve();
-					})
-					.on("error", err => {
-						mongoose.disconnect();
-						reject(err);
-					});
-			}
-		);
-	});
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
 }
 
 function closeServer() {
-	return mongoose.disconnect().then(() => {
-		return new Promise((resolve, reject) => {
-			console.log("Closing server");
-			server.close(err => {
-				if (err) {
-					return reject(err);
-				}
-				resolve();
-			});
-		});
-	});
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 if (require.main === module) {
-	runServer(DATABASE_URL).catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = { runServer, app, closeServer };
